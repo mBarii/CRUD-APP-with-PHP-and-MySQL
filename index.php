905bfe94bcda653c46e522df3e3809c579fe35cb
Jarry Cabaluna <janjalani.yonbanme01@gmail.com>
2022-10-25 18:39:07 +0800

    crud app finale

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Welcome to the home page! <br /> Where nothing is included at the moment... <br /> Enjoy the existing home page at the moment! <br /> ( ͡° ͜ʖ ͡° ) </p>
+</div>
+
+<?=template_footer()?>
